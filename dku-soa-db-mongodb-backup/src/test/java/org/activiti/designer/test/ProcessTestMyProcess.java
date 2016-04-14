32a6ade1c384f4b5ee9bc62d058037429a969d3a
DELL <DELL@DESKTOP-8QJI4JU>
2016-04-14 23:12:10 +0800

    xml spring

--- dku-soa-db-mongodb-backup/src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
+++ dku-soa-db-mongodb-backup/src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
@@ -8,18 +8,21 @@ import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.HistoryService;
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.history.HistoricVariableInstance;
-import org.activiti.engine.impl.history.HistoryLevel;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//@RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(locations = "classpath*:srping/*.xml")
 public class ProcessTestMyProcess {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProcessTestMyProcess.class);
 
@@ -28,32 +31,30 @@ public class ProcessTestMyProcess {
 
 	// @Rule
 	// public ActivitiRule activitiRule = new ActivitiRule();
+	// @Autowired
 	private RuntimeService runtimeService;
+	// @Autowired
 	private HistoryService historyService;
+	// @Autowired
 	private RepositoryService repositoryService;
 
 	@Before
 	public void before() {
-
-		ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault()
-				.setHistoryLevel(HistoryLevel.FULL).buildProcessEngine();
-
-		runtimeService = processEngine.getRuntimeService();
-		historyService = processEngine.getHistoryService();
-		repositoryService = processEngine.getRepositoryService();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/*.xml");
+		System.out.println("context");
 	}
 
 	@Test
 	public void startProcess() throws Exception {
-
 		repositoryService.createDeployment().addInputStream("transaction.bpmn20.xml", new FileInputStream(filename))
 				.deploy();
+
 		Map<String, Object> variableMap = new HashMap<String, Object>();
-		variableMap.put("rollback", Boolean.TRUE);
+		variableMap.put("rollback", false);
 		variableMap.put("from_balance", 3000);
 		variableMap.put("to_balance", 1000);
 		variableMap.put("amount", 100);
-		variableMap.put("error_event", true);
+		variableMap.put("error_event", false);
 
 		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("transactionProcess", variableMap);
 		assertNotNull(processInstance.getId());
